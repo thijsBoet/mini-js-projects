7acef2fa380ee00ba501097b4e77423edfe9dd2f
Matthijs Boet <m.boet@chello.nl>
2020-06-13 15:58:46 +0200

    finshed 1-color-flipper and 2-counter project

---
+++ 2-counter/app.js
@@ -0,0 +1,15 @@
+// set initial count
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', function(e) {
+    const styles = e.currentTarget.classList
+    if(styles.contains('decrease')) count--;
+    if(styles.contains('reset')) count = 0;
+    if(styles.contains('increase')) count++;
+    value.textContent = count;
+  })
+});
