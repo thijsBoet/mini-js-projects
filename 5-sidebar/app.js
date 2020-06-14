7a96f15534cc6315978f3efd8748cb42cf2383e1
Matthijs Boet <m.boet@chello.nl>
2020-06-14 15:20:54 +0200

    finished 6-modal

--- 5-sidebar/app.js
+++ 5-sidebar/app.js
@@ -2,10 +2,6 @@ const toggleBtn = document.querySelector('.sidebar-toggle');
 const closeBtn = document.querySelector('.close-btn');
 const sidebar = document.querySelector('.sidebar');
 
-toggleBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('show-sidebar');
-});
+toggleBtn.addEventListener('click', () => sidebar.classList.toggle('show-sidebar'));
 
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.remove('show-sidebar');
-});
+closeBtn.addEventListener('click', () => sidebar.classList.remove('show-sidebar'));
