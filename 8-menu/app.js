da1437262a22ae396d9258c3986cfacbb273d3ad
Matthijs Boet <m.boet@chello.nl>
2020-06-15 21:25:18 +0200

    finished 8-menu

--- 8-menu/app.js
+++ 8-menu/app.js
@@ -72,3 +72,45 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+// load items
+window.addEventListener('DOMContentLoaded', () => {
+  displayMenuItems(menu);
+});
+// filter items
+filterBtns.forEach((btn)=> {
+  btn.addEventListener('click', (e) => {
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter((menuItem) => {
+      if(menuItem.category === category){
+        return menuItem;
+      }
+    });
+    if (category === 'all') {
+      displayMenuItems(menu);
+    } else {
+      displayMenuItems(menuCategory);
+    }
+  });
+});
+
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map((item) => {
+    return `<article class="menu-item">
+              <img src="${item.img}" class="photo" alt="${item.title}">
+              <div class="item-info">
+                <header>
+                  <h4>${item.title}</h4>
+                  <h4 class="price">$${item.price}</h4>
+                </header>
+                <p class="item-text">${item.desc}</p>
+              </div>
+            </article>`
+  });
+  displayMenu = displayMenu.join('');
+  sectionCenter.innerHTML = displayMenu;
+}
