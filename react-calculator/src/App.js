7e6550e2c04f5772c119842343f33ffdd7b8115f
Matthijs Boet <m.boet@chello.nl>
2020-07-19 00:34:02 +0200

    started react-calculator\

---
+++ react-calculator/src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./App.css";
+import Button from './components/Button';
+
+
+class App extends Component {
+  state = {
+    
+  };
+  render() {
+    return (
+      <div className="app">
+        <div className="calc-wrapper">
+          <div className="row">
+            <Button>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button>/</Button>
+          </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button>X</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button>+</Button>
+          </div>
+          <div className="row">
+            <Button>.</Button>
+            <Button>0</Button>
+            <Button>=</Button>
+            <Button>-</Button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+}
+
+export default App;
+ 
---
+++ react-calculator/src/components/Button.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Button.css";
+
+const isOperator = (val) => {
+  return !isNaN(val) || val === "." || val === "=";
+};
+
+const Button = (props) => {
+  return (
+    <div
+      className={`button-wrapper 
+      ${isOperator(props.children)
+        ? null
+        : "operator"}`}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Button;
