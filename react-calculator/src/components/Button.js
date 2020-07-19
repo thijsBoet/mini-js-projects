731693eb14f6d94d3a358bc501e7247218b5f483
Matthijs Boet <m.boet@chello.nl>
2020-07-19 03:11:30 +0200

    finished react-calculator

--- react-calculator/src/components/Button.js
+++ react-calculator/src/components/Button.js
@@ -5,17 +5,16 @@ const isOperator = (val) => {
   return !isNaN(val) || val === "." || val === "=";
 };
 
-const Button = (props) => {
+export const Button = (props) => {
   return (
     <div
       className={`button-wrapper 
       ${isOperator(props.children)
         ? null
         : "operator"}`}
+      onClick={() => props.handleClick(props.children)}
     >
       {props.children}
     </div>
   );
 };
-
-export default Button;
